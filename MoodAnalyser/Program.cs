0a2811ebd91fe5cbffc97e82a99257587902aa5e
unknown <neeladri22@gmail.com>
2022-07-10 05:05:28 +0530

    Welcome to Mood Analyser Problem

---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MoodAnalyser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Problem");
+            Console.WriteLine("\n");
+     
+            
+        }
+    }
+}
