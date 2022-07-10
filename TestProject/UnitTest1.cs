d1eaeb82c4c0aad5ad27fa7c2a2e12bfcf8606a6
unknown <neeladri22@gmail.com>
2022-07-10 05:30:25 +0530

    I am in Happy Mood

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -10,8 +10,8 @@ namespace TestProject
         public void TestMethod1()
         {
             //Act
-            string expected = "SAD";
-            string message = "I am in sad Mood";
+            string expected = "HAPPY";
+            string message = "I am in happy mood";
 
             MoodAnalyse moodAnalyse = new MoodAnalyse(message);
 
