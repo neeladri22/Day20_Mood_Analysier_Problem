7d9499afce0c317af52d81788f10421375ce255d
unknown <neeladri22@gmail.com>
2022-07-10 08:12:40 +0530

    MoodAnalyser with Parameter Constructor

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -113,7 +113,7 @@ namespace TestProject
                 Assert.AreEqual(expected, exception.Message);
             }
         }
-         */
+         
         //UC4_TC4.3
         public void TestMethod1()
         {
@@ -127,8 +127,14 @@ namespace TestProject
                 Assert.AreEqual(expected, exception.Message);
             }
         }
-
-
+        */
+        //UC5 problem test
+        public void TestMethod1()
+        {
+            object expected = new MoodAnalyse("I a in Happy mood");
+            object obj = MoodAnalyserFactory.CreateMoodAnalyserUsingParametrisedConstructor("MoodAnalyse", "MoodAnalyse", "I a in Happy mood");
+            expected.Equals(obj);
+        }
     }
 
 }
