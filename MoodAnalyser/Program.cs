ceaa3cbcd461199b60e9708857696f679dd84de4
unknown <neeladri22@gmail.com>
2022-07-10 06:33:50 +0530

    Added Invalid Mood

--- MoodAnalyser/Program.cs
+++ MoodAnalyser/Program.cs
@@ -9,7 +9,11 @@ namespace MoodAnalyser
             Console.WriteLine("Welcome to Mood Analyser Problem");
             Console.WriteLine("\n");
 
-            string message = "I am in happy mood";
+           // string message = "I am in happy mood";
+           // MoodAnalyse moodAnalyse = new MoodAnalyse(message);
+           // moodAnalyse.AnalyseMood();
+
+            string message = null;
             MoodAnalyse moodAnalyse = new MoodAnalyse(message);
             moodAnalyse.AnalyseMood();
 
--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -7,6 +7,11 @@ namespace TestProject
     public class Test1
     {
         [TestMethod]
+
+        /*
+        
+        // UC1 Program
+        
         public void TestMethod1()
         {
             //Act
@@ -22,5 +27,21 @@ namespace TestProject
             //Assert
             Assert.AreEqual(expected, mood);
         }
+        */
+        public void TestMethod1()
+        {
+            //Act
+
+            string expected = "HAPPY";
+            string message = null;
+            MoodAnalyse moodAnalyse = new MoodAnalyse(message);
+
+            //Arrange
+            string mood = moodAnalyse.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected, mood);
+        }
     }
+
 }
